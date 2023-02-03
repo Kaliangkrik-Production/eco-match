89ce41e4018d9c6440675b1fe6431f90e415591c
furaihan <28698437+furaihan@users.noreply.github.com>
2023-02-03 21:57:12 +0700

    signup say

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,34 +1,30 @@
 export default function Signup() {
   return (
-    <div className="bg-blue-400">
-      <div className="flex flex-col items-center justify-center h-screen">
-        <div className="flex flex-col items-center justify-center bg-white rounded-lg shadow-lg p-8">
-          <h1 className="text-3xl font-bold mb-4">Sign Up</h1>
-          <div className="flex flex-col w-full">
-            <label className="mb-2">Email</label>
-            <input
-              className="border rounded-lg px-3 py-2 mb-4"
-              type="email"
-              placeholder="Email"
-            />
-            <label className="mb-2">Password</label>
-            <input
-              className="border rounded-lg px-3 py-2 mb-4"
-              type="password"
-              placeholder="Password"
-            />
-            <label className="mb-2">Confirm Password</label>
-            <input
-              className="border rounded-lg px-3 py-2 mb-4"
-              type="password"
-              placeholder="Confirm Password"
-            />
-            <button className="bg-blue-400 text-white rounded-lg px-3 py-2">
-              Sign Up
-            </button>
+    <div className="flex h-screen w-full flex-row bg-white" id="home">
+      <div className="flex flex-col w-1/2 bg-white">
+        <div className="flex flex-col items-center justify-center h-screen">
+          <div className="flex flex-col items-center justify-center bg-blue-400 rounded-lg shadow-lg p-8">
+            <h1 className="text-3xl font-bold mb-4">Sign up</h1>
+            <div className="flex flex-col w-full">
+              <label className="mb-2">Email</label>
+              <input className="border rounded-lg px-3 py-2 mb-4" type="email" placeholder="Email" />
+              <label className="mb-2">Username</label>
+              <input className="border rounded-lg px-3 py-2 mb-4" type="text" placeholder="Username" />
+              <label className="mb-2">Password</label>
+              <input className="border rounded-lg px-3 py-2 mb-4" type="password" placeholder="Password" />
+              <label className="mb-2">Confirm password</label>
+              <input className="border rounded-lg px-3 py-2 mb-4" type="password" placeholder="Confirm password" />
+              <div className="flex flex-row flex-auto">
+                <input type="checkbox" className="mr-2" id="agreeTOS" value="agreeTOS" name="agreeTOS" />
+                <label htmlFor="agreeTOS">I agree to the <a href="/tos" className="text-white">Terms of Service</a></label>
+              </div>
+              <button type="submit" className="bg-white text-black rounded-lg px-3 py-2 mt-4">Sign up</button>
+              <label className="mt-4">Already have an account? <a href="/login" className="text-white">Login</a></label>
+            </div>
           </div>
         </div>
       </div>
+      <div className="flex flex-col w-1/2 bg-black"/>
     </div>
   );
 }
