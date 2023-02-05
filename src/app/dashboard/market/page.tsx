2a76578856f84049f2fc98af840368201d824637
doddy-s <doddy.s@protonmail.com>
2023-02-05 19:00:48 +0700

    fixes routing

--- src/app/dashboard/market/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div>
-      <div></div>
-    </div>
-  );
-}
--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,10 +1,7 @@
-export default function Chuakks()
-{
-    return (
-        <iframe 
-        src="https://www.google.com/maps/d/embed?mid=1zZY_C5VhKuES6mpV3WN7GEu9hE9TOjw&ehbc=2E312F" 
-        width="640" 
-        height="480">
-        </iframe>
-    )
+export default function Page() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
 }
