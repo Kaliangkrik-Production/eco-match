1e5efb9875f77701f2974f57823c2ea1a48d7c33
doddy-s <doddy.s@protonmail.com>
2023-02-08 05:58:53 +0700

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,9 +37,60 @@ export default function Page() {
           />
         </div>
       </div>
-      <div className="h-screen w-screen bg-yellow-500" id="about"></div>
-      <div className="h-screen w-screen bg-green-500" id="market"></div>
-      <div className="h-screen w-screen bg-blue-500" id="map"></div>
+      <div className="h-auto min-h-screen p-24">
+        <div className="items-top flex flex-row justify-between">
+          <h1 className="mb-8 text-2xl font-bold">Popular Market</h1>
+          <a
+            href="/market"
+            className="text-xl"
+          >
+            See All
+          </a>
+        </div>
+        <div className="flex scroll-m-10 flex-row items-start justify-start gap-8 overflow-auto whitespace-nowrap">
+          <div className="aspect-video h-64 bg-black"></div>
+          <div className="aspect-video h-64 bg-black"></div>
+          <div className="aspect-video h-64 bg-black"></div>
+          <div className="aspect-video h-64 bg-black"></div>
+          <div className="aspect-video h-64 bg-black"></div>
+          <div className="aspect-video h-64 bg-black"></div>
+          <div className="aspect-video h-64 bg-black"></div>
+        </div>
+        <div className="flex h-auto w-full flex-col py-24 lg:flex-row">
+          {/*Kategori*/}
+          <div className="flex h-[40rem] w-full flex-col items-center justify-between lg:w-1/4">
+            <div className="items-top flex w-full flex-row justify-between">
+              <h1 className="mb-8 text-2xl font-bold">Categories</h1>
+              <a
+                href="/categories"
+                className="text-xl"
+              >
+                See All
+              </a>
+            </div>
+            <div className="h-full w-full rounded-lg bg-gray-400"></div>
+          </div>
+
+          {/*Edukasi*/}
+          <div className="flex h-[40rem] w-full flex-col items-center justify-between py-8 px-0 lg:w-1/2 lg:py-0 lg:px-8">
+            <div className="items-top flex w-full flex-row justify-between">
+              <h1 className="mb-8 text-2xl font-bold">Education</h1>
+              <a
+                href="/education"
+                className="text-xl"
+              >
+                See All
+              </a>
+            </div>
+            <div className="h-full w-full rounded-lg bg-gray-400"></div>
+          </div>
+
+          {/*Promo*/}
+          <div className="flex h-[40rem] w-full flex-col items-center justify-between lg:w-1/4">
+            <div className="h-full w-full rounded-lg bg-gray-400"></div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </main>
   );
