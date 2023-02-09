84ec9ed89d488ee708804cec41210d3dd54ea442
furaihan <28698437+furaihan@users.noreply.github.com>
2023-02-09 19:00:36 +0700

    account part 2

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/Footer";
 import NavBar from "@/components/NavBar";
+import { Combobox } from "@headlessui/react";
 
 export default function Page() {
   return (
@@ -12,7 +13,91 @@ export default function Page() {
             <div className="flex h-[60%] w-full flex-row items-center justify-center rounded-t-xl bg-red-400" />
             <div className="relative bottom-[20%] h-20 w-20 rounded-full bg-blue-400" />
           </div>
-          <div className="mt-10 flex h-screen w-[95%] flex-col rounded-md bg-teal-500"></div>
+          <div className="mt-10 grid h-screen w-[95%] grid-cols-2 gap-12 rounded-md bg-teal-500 p-8">
+            <div className="flex flex-col">
+              <label className="text-l mb-5 font-bold">First Name</label>
+              <input
+                className="h-10 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 "
+                placeholder="Nama"
+                type="text"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label className="text-l mb-5 font-bold">Last Name</label>
+              <input
+                className="h-10 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+                placeholder="Nama"
+              />
+            </div>
+            <div className="col-span-2 flex flex-col">
+              <label className="text-l mb-5 font-bold">Email</label>
+              <input
+                className="h-10 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+                placeholder="Email"
+              />
+            </div>
+            <div className="col-span-2 flex flex-col">
+              {/* DOB */}
+              <label className="text-l mb-5 font-bold">Date of Birth</label>
+              <input
+                className="h-10 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+                placeholder="Tanggal Lahir"
+              />
+            </div>
+            <div className="flex flex-col">
+              {/* Phone Number */}
+              <label className="text-l mb-5 font-bold">Phone Number</label>
+              <input
+                className="h-10 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+                placeholder="Nomor Telepon"
+                id="phone"
+              />
+            </div>
+            <div className="flex flex-col">
+              {/* Gender */}
+              <label className="text-l mb-5 font-bold">Gender</label>
+              <div className="flex h-10 flex-col items-center justify-center rounded-xl border border-gray-500">
+                <ul className="flex flex-row items-center justify-center gap-4">
+                  <li>
+                    <div className="flex flex-row gap-2">
+                      <input
+                        id="male"
+                        type="radio"
+                        value=""
+                        name="gender-radio"
+                        className="h-4 w-4"
+                      />
+                      <label htmlFor="male">Male</label>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="flex flex-row gap-2">
+                      <input
+                        id="female"
+                        type="radio"
+                        value=""
+                        name="gender-radio"
+                        className="h-4 w-4"
+                      />
+                      <label htmlFor="female">Female</label>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="flex flex-row gap-2">
+                      <input
+                        id="none"
+                        type="radio"
+                        value=""
+                        name="gender-radio"
+                        className="h-4 w-4"
+                      />
+                      <label htmlFor="none">None</label>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
