b82c4073eb4da2c75dc21c48ce2d8627ffee5477
furaihan <28698437+furaihan@users.noreply.github.com>
2023-02-09 23:09:54 +0700

    account lagi

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -8,12 +8,12 @@ export default function Page() {
       <NavBar />
       <div className="mb-10 flex h-full w-full flex-col items-center justify-center bg-white">
         <h1 className="mb-5 mt-16 text-4xl font-bold">User Profile</h1>
-        <div className="mb-10 flex h-[50rem] w-[90%] flex-col items-center justify-center rounded-xl pb-10 shadow-2xl shadow-black">
+        <div className="mb-10 flex h-[79rem] w-[90%] flex-col items-center justify-center rounded-xl pb-10 shadow-2xl shadow-black">
           <div className="flex h-[30%] w-full flex-col items-center justify-center">
             <div className="flex h-[60%] w-full flex-row items-center justify-center rounded-t-xl bg-red-400" />
             <div className="relative bottom-[20%] h-20 w-20 rounded-full bg-blue-400" />
           </div>
-          <div className="mt-10 grid h-screen w-[95%] grid-cols-2 gap-12 rounded-md bg-teal-500 p-8">
+          <div className="mt-10 grid h-[65rem] w-[95%] grid-cols-2 gap-12 rounded-md bg-teal-500 p-8">
             <div className="flex flex-col">
               <label className="text-l mb-5 font-bold">First Name</label>
               <input
@@ -96,6 +96,38 @@ export default function Page() {
                   </li>
                 </ul>
               </div>
+            </div>
+            <div className="flex flex-col col-span-2">
+              <label className="text-l mb-5 font-bold">Address</label>
+              <input
+                  className="h-20 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+                  placeholder="Alamat"
+                  id="address"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label className="text-l mb-5 font-bold">Province</label>
+              <input
+                  className="h-10 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+                  placeholder="Provinsi"
+                  id="provinsi"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label className="text-l mb-5 font-bold">City</label>
+              <input
+                  className="h-10 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+                  placeholder="Kota"
+                  id="city"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label className="text-l mb-5 font-bold">Zipcode</label>
+              <input
+                  className="h-10 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+                  placeholder="Kode Pos"
+                  id="zipcode"
+              />
             </div>
           </div>
         </div>
