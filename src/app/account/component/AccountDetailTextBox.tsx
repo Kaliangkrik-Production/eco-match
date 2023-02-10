6bc60758ad95d03d732649ed24ddc421828fce73
furaihan <28698437+furaihan@users.noreply.github.com>
2023-02-11 01:31:25 +0700

    fragment

--- src/app/account/component/AccountDetailTextBox.tsx
+++ src/app/account/component/AccountDetailTextBox.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 interface AccountDetailTextBoxProps {
   labelText: string;
   placeholder: string;
@@ -5,13 +7,13 @@ interface AccountDetailTextBoxProps {
 
 export default function AccountDetailTextBox(props: AccountDetailTextBoxProps) {
   return (
-    <div className="flex flex-col">
+    <React.Fragment>
       <label className="text-l mb-5 font-bold">{props.labelText}</label>
       <input
         className="h-10 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900"
         type="text"
         placeholder={props.placeholder}
       />
-    </div>
+    </React.Fragment>
   );
 }
